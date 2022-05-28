4723db60a9ad2cdcaf30dcfe4359d09a953cefee
Jiangliquan <Jliquan32@163.com>
2022-05-28 15:23:24 +0800

    迭代器生成器，函数，输入输出

---
+++ base/10Function.py
@@ -0,0 +1,14 @@
+# https://www.runoob.com/python3/python3-function.html
+
+def area(width,height):
+    return width * height
+
+def print_welcome(name):
+    print("Welcome",name)
+
+print_welcome("Jace")
+w = 6
+h = 8
+print("width = ", w , "height = ", h , "area = ", area(w,h))
+
+
